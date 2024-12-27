56e821d08395a00c23335c393850356e4c5e9f87
milyasbpa <basilyas.2015@gmail.com>
2024-12-27 23:27:14 +0700

    fix: remove controller and app service

--- tree-structure-backend/src/app.module.ts
+++ tree-structure-backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MenuModule } from './infrastructure/modules/menu/menu.module';
 
 @Module({
   imports: [MenuModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
