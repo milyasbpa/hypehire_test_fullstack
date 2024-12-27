88b1bd2571aff495af4035796b4f6837fdffbd7a
milyasbpa <basilyas.2015@gmail.com>
2024-12-27 23:42:37 +0700

    fix: bootstrap

--- tree-structure-backend/src/main.ts
+++ tree-structure-backend/src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+// import * as express from 'express';
+import * as serverless from 'serverless-http';
 
-const server = express();
+// const server = express();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+// export const handler = server;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  app.enableCors(); // Add this if you need to allow cross-origin requests
+  await app.init();
 
-export const handler = server;
+  // Serverless wrapper for NestJS
+  return serverless(app.getHttpServer());
+}
+
+export const handler = bootstrap();
