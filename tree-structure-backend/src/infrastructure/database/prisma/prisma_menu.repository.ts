401f0fd5635df5aab36f742105f7538884206c83
milyasbpa <basilyas.2015@gmail.com>
2024-12-27 17:51:38 +0700

    fix: add uuid on backend

--- tree-structure-backend/src/infrastructure/database/prisma/prisma_menu.repository.ts
+++ tree-structure-backend/src/infrastructure/database/prisma/prisma_menu.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { MenuRepository } from '../../../core/domain/menu.repository.interface';
 import { Menu } from '../../../core/domain/menu.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class PrismaMenuRepository implements MenuRepository {
@@ -16,7 +17,15 @@ export class PrismaMenuRepository implements MenuRepository {
   }
 
   async create(menu: Menu): Promise<Menu> {
-    return this.prisma.menu.create({ data: menu });
+    return this.prisma.menu.create({
+      data: {
+        ...menu,
+        id: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        depth: !menu.parentId ? 1 : menu.depth,
+      },
+    });
   }
 
   async update(id: string, menu: Partial<Menu>): Promise<Menu> {
