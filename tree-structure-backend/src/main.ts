65c00428fb7a52759ef71dd7ce5d75b41760fc98
milyasbpa <basilyas.2015@gmail.com>
2024-12-27 23:58:07 +0700

    fix: redeploy

--- tree-structure-backend/src/main.ts
+++ tree-structure-backend/src/main.ts
@@ -1,24 +1,35 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// // import * as express from 'express';
+// import * as serverless from 'serverless-http';
+
+// // const server = express();
+// // async function bootstrap() {
+// //   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+// //   await app.listen(process.env.PORT ?? 3000);
+// // }
+// // bootstrap();
+
+// // export const handler = server;
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter());
+//   app.enableCors(); // Add this if you need to allow cross-origin requests
+//   await app.init();
+
+//   // Serverless wrapper for NestJS
+//   return serverless(app.getHttpServer());
+// }
+
+// export const handler = bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-// import * as express from 'express';
-import * as serverless from 'serverless-http';
+import { Server } from 'http';
 
-// const server = express();
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
-// export const handler = server;
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter());
-  app.enableCors(); // Add this if you need to allow cross-origin requests
-  await app.init();
-
-  // Serverless wrapper for NestJS
-  return serverless(app.getHttpServer());
+async function bootstrap(): Promise<Server> {
+  const app = await NestFactory.create(AppModule);
+  await app.init(); // Initialize without starting a server listener
+  return app.getHttpAdapter().getInstance(); // Return the HTTP server instance
 }
 
-export const handler = bootstrap();
+export default bootstrap;
