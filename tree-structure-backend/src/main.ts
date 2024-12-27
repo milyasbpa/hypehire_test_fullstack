aee04b1e7c47e9568e23f3c046b68e46d0e7f007
milyasbpa <basilyas.2015@gmail.com>
2024-12-28 01:18:53 +0700

    fix: change to default

--- tree-structure-backend/src/main.ts
+++ tree-structure-backend/src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Server } from 'http';
 
-// Serverless bootstrap function for platforms like Vercel
-async function bootstrap(): Promise<Server> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init(); // Initialize without starting a listener
-  return app.getHttpAdapter().getInstance(); // Return the HTTP server instance
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-// // Local development entry point
-// async function localBootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   const port = process.env.PORT || 3000;
-//   console.log(`Server running locally on http://localhost:${port}`);
-//   await app.listen(port);
-// }
-
-// Export serverless function for Vercel
-export default bootstrap;
-
-// // Run the local server if executed directly
-// if (require.main === module) {
-//   localBootstrap();
-// }
+bootstrap();
